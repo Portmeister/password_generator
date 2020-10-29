d1190f0e16f2862cd63b797b8b44d2d819216332
Portmeister <portmeister@gmail.com>
2020-10-29 13:06:41 -0500

    update relative file paths in index and define character variables in script

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,8 @@
 // Assignment Code
+var UpperCase = confirm("Would you like your password to contain uppercase letters?")
+var lowerCase = confirm("Would you like your password to contain lowercase letters?")
+var numbers = confirm("Would you like your password to contain numbers?")
+var characters = confirm("Would you like your password to contain special characters?")
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
